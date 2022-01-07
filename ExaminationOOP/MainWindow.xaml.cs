e7ec5ea78f35434251bb37a2a5e7d680cf5da543
Mohamad Al-Darraji <60218193+S00199770@users.noreply.github.com>
2022-01-07 18:48:57 +0000

    Uploaded
    Could not upload during work using the VM

---
+++ ExaminationOOP/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExaminationOOP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // ObservableCollection<Member> memberList = new ObservableCollection<Member>();
+        List<Member> memberList = new List<Member>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            JuniorMember jm1 = new JuniorMember() { Name = "Jack Murphy", Fee = 1000, JoinDate = new DateTime(2020, 5, 7), PaymentType = PaymentSchedule.Annual };
+            JuniorMember jm2 = new JuniorMember() { Name = "Emily Kelly", Fee = 1000, JoinDate = new DateTime(2021, 1, 10), PaymentType = PaymentSchedule.Biannual };
+            JuniorMember jm3 = new JuniorMember() { Name = "Daniel Ryan", Fee = 1000, JoinDate = new DateTime(2020, 11, 30), PaymentType = PaymentSchedule.Monthly };
+
+            Member m1 = new Member() { Name = "Ella Doyle", Fee = 500, JoinDate = new DateTime(2019, 3, 20), PaymentType = PaymentSchedule.Annual };
+            Member m2 = new Member() { Name = "Fionn Kennedy", Fee = 500, JoinDate = new DateTime(2018, 8, 15), PaymentType = PaymentSchedule.Biannual };
+            Member m3 = new Member() { Name = "Louise Moore", Fee = 500, JoinDate = new DateTime(2017, 2, 10), PaymentType = PaymentSchedule.Monthly };
+
+            SeniorMember sm1 = new SeniorMember() { Name = "Cian Daly", Fee = 750, JoinDate = new DateTime(2015, 4, 24), PaymentType = PaymentSchedule.Annual };
+            SeniorMember sm2 = new SeniorMember() { Name = "Bobby Quinn", Fee = 750, JoinDate = new DateTime(2014, 12, 1), PaymentType = PaymentSchedule.Biannual };
+            SeniorMember sm3 = new SeniorMember() { Name = "Eve Gallagher", Fee = 750, JoinDate = new DateTime(2013, 6, 1), PaymentType = PaymentSchedule.Monthly };
+
+
+
+            memberList.Add(m1);
+            memberList.Add(m2);
+            memberList.Add(m3);
+            memberList.Add(jm1);
+            memberList.Add(jm2);
+            memberList.Add(jm3);
+            memberList.Add(sm1);
+            memberList.Add(sm2);
+            memberList.Add(sm3);
+            memberListLstbx.ItemsSource = memberList;
+        }
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ExaminationOOP/Member.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExaminationOOP
+{
+    public enum PaymentSchedule { Annual, Biannual, Monthly };
+    class Member
+    {
+        public DateTime JoinDate;
+        public Decimal Fee;
+        public PaymentSchedule PaymentType;
+        public String Name;
+        public PaymentSchedule paymentSchedule { get; set; }
+        private DateTime renewalDate;
+
+        /*    private DateTime daysToRenewal;
+            public DateTime DaysToRenewal
+            {
+                get
+                {
+                    daysToRenewal = renewalDate - DateTime.Now;
+                    return daysToRenewal;
+                }
+            }
+
+        */
+        public DateTime RenewalDate
+        {
+            get
+            {
+                renewalDate = JoinDate.AddMonths(1);
+                return renewalDate;
+            }
+        }
+        public decimal CalculateFees(decimal fee)
+        {
+            if (PaymentType == PaymentSchedule.Monthly)
+                fee = Fee / 12;
+            else if (PaymentType == PaymentSchedule.Biannual)
+                fee = Fee / 2;
+            else if (PaymentType == PaymentSchedule.Annual)
+                fee = Fee / 1;
+
+            return fee;
+        }
+        public override string ToString()
+        {
+            return $"{Name}";
+       }
+    }
+}
